81920a9b77d665a72959a8235c40580fe4948a9e
Victor Martinez <vicmarc30@gmail.com>
2026-02-25 16:05:56 -0600

    First commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProductController extends AbstractController
+{
+    #[Route('/admin/view/product', name: 'app_product')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/admin/add/product', name: 'app_product_add')]
+    public function add(Request $request, EntityManagerInterface $entityManager)
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product->setCreatedAt(new \DateTimeImmutable());
+        }
+        return $this->render('product/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+}
